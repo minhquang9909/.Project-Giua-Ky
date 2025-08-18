41d3964aa45989557aaf4b774e70bf3c91f01dfc
minhquang9909 <minhquang9909@gmail.com>
2025-08-18 17:58:13 +0700

    ProJect!!!

--- Detail/Detail.js
+++ Detail/Detail.js
@@ -18,3 +18,33 @@ document.querySelector("#navlogo").addEventListener("click", function() {
     window.location.href = "../HomePage.html";
 });
 // ------------p3------------
+document.addEventListener("DOMContentLoaded", function() {
+    const smallPics = Array.from(document.querySelectorAll('.p32p'));
+    const mainPic = document.querySelector('.p31m');
+    const leftBtn = document.querySelector('.p31l');
+    const rightBtn = document.querySelector('.p31r');
+    let i = 0;
+
+    function updateMainPic() {
+        mainPic.src = smallPics[i].src;
+    }
+
+    leftBtn.addEventListener('click', function() {
+        i = (i - 1 + smallPics.length) % smallPics.length;
+        updateMainPic();
+    });
+
+    rightBtn.addEventListener('click', function() {
+        current = (current + 1) % smallPics.length;
+        updateMainPic();
+    });
+
+    smallPics.forEach((img, idx) => {
+        img.addEventListener('click', function() {
+            current = idx;
+            updateMainPic();
+        });
+    });
+
+    updateMainPic();
+});
